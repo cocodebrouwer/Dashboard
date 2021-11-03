456827bb5113da4c1fcc8614e8c41c4e68c47faf
cocodebrouwer <91317848+cocodebrouwer@users.noreply.github.com>
2021-11-03 15:32:22 +0100

    Update Dashboard-app.py

--- Dashboard-app.py
+++ Dashboard-app.py
@@ -150,15 +150,15 @@ fig5 = px.scatter(data1,
                   title = 'Nederlandse luchthavens en sterftecijfers 2020-2021')
 
 #Dropdown buttons
-dropdown_buttons = [{'label':"Luchthavens NL", 'method':"update", 'args':[{"visible":[True, True, True, True, True]}]},
-                    {'label':"Amsterdam Airport Schiphol", 'method':"update", 'args':[{"visible":[True, False, False, False, False]}]},
-                    {'label':"Rotterdam The Hague Airport", 'method':"update", 'args':[{"visible":[False, True, False, False, False]}]},
-                    {'label':"Eindhoven Airport", 'method':"update", 'args':[{"visible":[False, False, True, False, False]}]}, 
-                    {'label':"Maastricht Aachen Airport", 'method':"update", 'args':[{"visible":[False, False, False, True, False]}]}, 
-                    {'label':"Groningen Airport Eelde", 'method':"update", 'args':[{"visible":[False, False, False, False, True]}]}]
+#dropdown_buttons = [{'label':"Luchthavens NL", 'method':"update", 'args':[{"visible":[True, True, True, True, True]}]},
+#                     {'label':"Amsterdam Airport Schiphol", 'method':"update", 'args':[{"visible":[True, False, False, False, False]}]},
+#                     {'label':"Rotterdam The Hague Airport", 'method':"update", 'args':[{"visible":[False, True, False, False, False]}]},
+#                     {'label':"Eindhoven Airport", 'method':"update", 'args':[{"visible":[False, False, True, False, False]}]}, 
+#                     {'label':"Maastricht Aachen Airport", 'method':"update", 'args':[{"visible":[False, False, False, True, False]}]}, 
+#                     {'label':"Groningen Airport Eelde", 'method':"update", 'args':[{"visible":[False, False, False, False, True]}]}]
 
-#Update de figuur
-fig5.update_layout({'updatemenus':[{'active':0, 'buttons':dropdown_buttons}]})
+# #Update de figuur
+# fig5.update_layout({'updatemenus':[{'active':0, 'buttons':dropdown_buttons}]})
 
 #Laat de figuur zien
 st.plotly_chart(fig5)
