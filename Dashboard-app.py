eea767b91c5aa32575f2e0516cbb94e4fdb4411b
cocodebrouwer <91317848+cocodebrouwer@users.noreply.github.com>
2021-11-03 15:41:22 +0100

    Update Dashboard-app.py

--- Dashboard-app.py
+++ Dashboard-app.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
-import statsmodels.api as sm
+import statsmodels
 
 st.title("✈️ Dashboard Eindpresentatie ✈️")
 
