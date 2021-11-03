2fa4fa812515d93fab3f9df9f1a4d71232abc535
cocodebrouwer <91317848+cocodebrouwer@users.noreply.github.com>
2021-11-03 16:45:52 +0100

    Update Dashboard-app.py

--- Dashboard-app.py
+++ Dashboard-app.py
@@ -174,5 +174,4 @@ elif nav == "Kaart":
                  title = 'Nederlandse luchthavens en COVID-19', 
                  fitbounds = 'locations', 
                  size_max = 100)
-  fig7.update_layout(transition = {'duration': 10000})
   st.plotly_chart(fig7)
